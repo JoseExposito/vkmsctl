3bd4861febe590a2b6ad9b629601177344d6dd01
José Expósito <jose.exposito89@gmail.com>
2024-05-17 12:38:48 +0200

    Add command line argument parser
    Use "clap" to parse the command line arguments.

---
+++ src/args_parser.rs
@@ -0,0 +1,22 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Directory were ConfigFS is mounted/needs to be mounted.
+    #[arg(long, default_value = "/config")]
+    pub configfs_path : String,
+
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Display current configuration.
+    Config {},
+}
+
+pub fn parse() -> Args {
+    Args::parse()
+}
